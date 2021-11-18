4c2e1884d9ba84d8fd2174fa4615fcdfd02df855
min <0s.dongmin@gmail.com>
2021-11-19 01:31:56 +0900

    Add ZoomableGroup

--- src/OngoingConflictView/OngoingConflictMap.tsx
+++ src/OngoingConflictView/OngoingConflictMap.tsx
@@ -3,7 +3,9 @@ import {
   ComposableMap,
   Geographies,
   Geography,
-  Sphere, Graticule,
+  Sphere,
+  Graticule,
+  ZoomableGroup,
 } from 'react-simple-maps'
 import { TConflictInfo } from './OngoingConflictView'
 import geography from '../data/geography.json'
@@ -32,41 +34,44 @@ const OngoingConflictMap = ({setConflictInfo}: TMapChartProps) => {
   return (
     <>
       <ComposableMap projectionConfig={{rotate: [-10, 0, 0], scale: 147}} width={800} height={400}>
-        <Sphere id="rsm-sphere" stroke="#F0F3FA" strokeWidth={0.5} fill="transparent"/>
-        <Graticule stroke="#F0F3FA" strokeWidth={0.5}/>
-        <Geographies geography={geography}>
-          {({geographies}) =>
-            geographies.map(geo => {
-              const {NAME} = geo.properties
-              const spareCoutries: string[] = (geographyCountryNameMap as any)[NAME] ?? []
-              const conflicts = [NAME, ...spareCoutries].map(key => ongoingArmedConflictMap[key]).filter(e => e).flat()
-              return <Geography
-                key={geo.rsmKey}
-                geography={geo}
-                onMouseEnter={() => {
-                  setConflictInfo(conflicts.length ? {name: NAME, conflicts} : undefined)
-                }}
-                onMouseLeave={() => {
-                  setConflictInfo(undefined)
-                }}
-                style={{
-                  default: {
-                    stroke: conflicts.length ? '#FFCCC7' : '#DADFE8',
-                    fill: conflicts.length ? '#FFF1F0' : '#FFFFFF',
-                  },
-                  hover: {
-                    fill: '#F53',
-                    outline: 'none',
-                  },
-                  pressed: {
-                    fill: '#E42',
-                    outline: 'none',
-                  }
-                }}
-              />
-            })
-          }
-        </Geographies>
+        <ZoomableGroup maxZoom={5} translateExtent={[[0, 0], [800, 400]]}>
+          <Sphere id="rsm-sphere" stroke="#F0F3FA" strokeWidth={0.5} fill="transparent"/>
+          <Graticule stroke="#F0F3FA" strokeWidth={0.5}/>
+          <Geographies geography={geography}>
+            {({geographies}) =>
+              geographies.map(geo => {
+                const {NAME} = geo.properties
+                const spareCoutries: string[] = (geographyCountryNameMap as any)[NAME] ?? []
+                const conflicts = [NAME, ...spareCoutries].map(key => ongoingArmedConflictMap[key]).filter(e => e).flat()
+                return <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  onMouseEnter={() => {
+                    setConflictInfo(conflicts.length ? {name: NAME, conflicts} : undefined)
+                  }}
+                  onMouseLeave={() => {
+                    setConflictInfo(undefined)
+                  }}
+                  style={{
+                    default: {
+                      stroke: conflicts.length ? '#FFCCC7' : '#DADFE8',
+                      fill: conflicts.length ? '#FFF1F0' : '#FFFFFF',
+                      strokeWidth: 0.5,
+                    },
+                    hover: {
+                      fill: '#F53',
+                      outline: 'none',
+                    },
+                    pressed: {
+                      fill: '#E42',
+                      outline: 'none',
+                    }
+                  }}
+                />
+              })
+            }
+          </Geographies>
+        </ZoomableGroup>
       </ComposableMap>
     </>
   )
