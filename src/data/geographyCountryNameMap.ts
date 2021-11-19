060a95f594bdcbcdd17babfd9b0560bfe1a076dc
min <0s.dongmin@gmail.com>
2021-11-19 15:22:58 +0900

    Mapping missing countries

--- src/data/geographyCountryNameMap.ts
+++ src/data/geographyCountryNameMap.ts
@@ -1,6 +1,17 @@
 const geographyCountryNameMap = {
   Azerbaijan: ['Artsakh'],
-  Israel: ['Gaza Strip']
+  Israel: ['Gaza Strip'],
+  Madagascar: ['Comoros', 'Mauritius', 'Maldives', 'Seychelles'],
+  'South Africa': ['Eswatini'],
+  Malaysia: ['Singapore'],
+  Qatar: ['Bahrain'],
+  'Dominican Rep.': ['Dominica'],
+  Fiji: ['Tonga', 'Tokelau'],
+  Moldova: ['Pridnestrovian Moldavian Republic'],
+  Georgia: ['Rep. Abkhazia', 'S. Ossetia'],
+  'Solomon Is.': ['Marshall Is.'],
+  Cameroon: ['Ambazonia'],
+  Nigeria: ['Biafra'],
 }
 
 export default geographyCountryNameMap
